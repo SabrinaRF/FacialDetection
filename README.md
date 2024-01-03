9b36e87bdb6b0e260614d6e1e0b2414dcc0c27e0
sabrina <sabrinarfernandes.srf@gmail.com>
2024-01-02 21:27:57 -0300

    Detecção facial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FacialDetection
 Utilizando  Java e o OpenCV para detectar faces em imagens e pela webcam
+
+* OpenCV: Open Source Computer Vision é uma biblioteca de programação voltada para o desenvolvimento de aplicativos de visão computacional. Ela fornece uma variedade de ferramentas e algoritmos para processamento de imagem e visão computacional em tempo real.
---
+++ src/deteccao/Exemplo1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package deteccao;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import static org.opencv.imgcodecs.Imgcodecs.imread;
+import org.opencv.imgproc.Imgproc;
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import org.opencv.objdetect.CascadeClassifier;
+
+/**
+ *
+ * @author sabrina
+ */
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat imgColor = imread("src\\pessoas\\teste.jpg");
+        Mat imgCinza = new Mat();
+        Imgproc.cvtColor(imgColor, imgCinza, COLOR_BGR2GRAY);
+        
+        CascadeClassifier classificador = new CascadeClassifier("src\\cascades\\haarcascade_frontalface_default.xml");
+        MatOfRect facesDetectadas = new  MatOfRect();
+        
+        classificador.detectMultiScale(imgCinza,facesDetectadas);
+        System.out.println(facesDetectadas.toArray().length);
+        
+        for(Rect rect :facesDetectadas.toArray()){
+            System.out.println(rect.x+" "+rect.y+" "+rect.width+" "+rect.height+" ");
+            Imgproc.rectangle(imgColor, 
+                new Point(rect.x,rect.y),
+                new Point(rect.x+rect.width,rect.y+rect.height),
+                new Scalar(0,0,255),
+        2
+            );
+        }
+        Utilitarios ut = new Utilitarios();
+        ut.mostraImagem(ut.convertMatToImage(imgColor));
+        
+    }
+}
